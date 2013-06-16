9c9e3451c7e51cc486a2a48617eef0c17d93ed43
Henry Huang <keenhenry1109@gmail.com>
2013-06-16 18:50:41 +0200

    added time period options in the cmd line interface

--- src/pda.py
+++ src/pda.py
@@ -15,7 +15,7 @@ def controller():
     p = argparse.ArgumentParser(
             description = 'A Personal Desktop Assistant to manage useful lists, like TODO list.',
             prog        = 'pda',
-            usage       = '%(prog)s [-c] lists'
+            usage       = '%(prog)s [-c|-d|-w|-m|-s|-y] lists'
         )
 
     #========================#
@@ -42,15 +42,21 @@ def controller():
     # p.add_option('--add','-a', action="store_true", help='gets current IP Address')
     # p.add_option('--update','-u', action="store_true", help='gets current IP Address')
     # p.add_option('--priority','-p', action="store_true", help='gets current IP Address')
-    # p.add_option('--delete','-d', action="store_true", help='gets current IP Address')
+    # p.add_option('--remove','-r', action="store_true", help='gets current IP Address')
 
     # other options
     # p.add_option('--verbose', '-v', action = 'store_true', help='prints verbosely', default=False)
     p.add_argument('--version', action='version', version='%(prog)s 0.1')
 
-    args = p.parse_args()
+    # time period optional arguments
+    p.add_argument('-d', action='store_true', help='daily content in a list')       # daily
+    p.add_argument('-w', action='store_true', help='weekly content in a list')      # weekly
+    p.add_argument('-m', action='store_true', help='monthly content in a list')     # monthly
+    p.add_argument('-s', action='store_true', help='seasonally content in a list')  # seasonally
+    p.add_argument('-y', action='store_true', help='yearly content in a list')      # yearly
 
     # debugging message for options and arguments
+    args = p.parse_args()
     print args
 
     #=============================================================#
@@ -61,12 +67,24 @@ def controller():
             print 'create lists:', args.lists
         else:
             # throw an error
+            # you probably need an full blown Error object
             print 'no lists to be created'
     elif args.lists:
-        # no optional arguments at all, only positional argument exists
-        # this default behavior for this situation is to display contents of
-        # lists stored in the database
-        print 'show lists:', args.lists
+        # this default behavior is to display contents of
+        # lists stored in the database, the following conditional
+        # branches can be implemented in DB model instead of here
+        if args.d:
+            print 'show daily contents in lists:', args.lists
+        if args.w:
+            print 'show weekly contents in lists:', args.lists
+        if args.m:
+            print 'show monthly contents in lists:', args.lists
+        if args.s:
+            print 'show seasonally contents in lists:', args.lists
+        if args.y:
+            print 'show yearly contents in lists:', args.lists
+        if not (args.d or args.w or args.m or args.s or args.y):
+            print 'show daily and weekly contents (default) in lists:', args.lists
     else:
         p.print_help()
 
