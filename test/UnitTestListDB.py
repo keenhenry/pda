3d6a2e4d7205bc2bee6818b24f8f27edaa29b85b
Henry Huang <keenhenry1109@gmail.com>
2013-12-25 17:46:06 +0100

    renamed unittest_pda.py to UnitTestListDB.py

---
+++ test/UnitTestListDB.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""
+This is a module to unit-test `ListDB` class.
+"""
+
+import unittest
+
+class ListDBTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.db = ListDB()
+
+    def testHasTaskFalse(self):
+        self.assertFalse(self.db._has_task(-1))
+        self.assertFalse(self.db._has_task(-2))
+        self.assertFalse(self.db._has_task('why'))
+
+    def testHasTaskTrue(self):
+        self.assertFalse(self.db._has_task(4))
+        self.assertFalse(self.db._has_task(5))
+        self.assertFalse(self.db._has_task(6))
+        self.assertFalse(self.db._has_task(8))
+
+    @classmethod
+    def tearDownClass(cls):
+        #cls._db = ListDB()
+        # do nothing for now
+        print
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
