fcba474e0b9187487e17b5c426e344b16dfea26a
Henry Huang <keenhenry1109@gmail.com>
2014-01-12 19:58:58 +0100

    implemented add_task(), read_tasks() and edit_task() methods

--- listdb/GithubIssues.py
+++ listdb/GithubIssues.py
@@ -3,122 +3,12 @@
 """
 `ListDB` is a data model abstraction of the list databse used by `pda`.
 
-================
-Helper Functions
-================
-
-=======
-gettors
-=======
-
-=======
-settors
-=======
-
 """
 
 import requests
 import os
 import shelve
 
-# def convert_priority_to_text(priority):
-#     """
-#     :param priority: integer
-#     :rtype: string
-#     """
-#     prio = ''
-# 
-#     if   priority == URGENT_MUSTDO: 
-#         prio = 'urgmust'
-#     elif priority == MUSTDO:
-#         prio = 'must'
-#     elif priority == HIGH_IMPORTANCE: 
-#         prio = 'high'
-#     elif priority == MEDIUM_IMPORTANCE: 
-#         prio = 'medium'
-#     else: 
-#         prio = 'low'
-# 
-#     return prio
-# 
-# def convert_milestone_title(time):
-#     """
-#     :param time: string
-#     :rtype: string
-#     """
-#     title = ''
-# 
-#     if   time == 'd':
-#         title = 'day'
-#     elif time == 'w':
-#         title = 'week'
-#     elif time == 'm':
-#         title = 'month'
-#     elif time == 's':
-#         title = 'season'
-#     else            :
-#         title = 'year'
-# 
-#     return title
-# 
-# def is_label_created(db, label_name):
-#     """
-#     :param db: :class:`github.Repository.Repository`
-#     :param label_name: string
-#     :rtype: Boolean
-#     """
-#     created = False
-# 
-#     for label in db.get_labels():
-#         if label.name == label_name:
-#             created = True
-#             break
-# 
-#     return created
-# 
-# def get_milestone_number(db, milestone_name):
-#     """
-#     :param db: :class:`github.Repository.Repository`
-#     :param milestone_name: string
-#     :rtype: None or integer
-#     """
-#     number = None
-# 
-#     for milestone in db.get_milestones():
-#         if milestone.title == milestone_name:
-#             number = milestone.number
-#             break
-# 
-#     return number
-# 
-# def get_issue_number(db, issue_number):
-#     """
-#     :param db: :class:`github.Repository.Repository`
-#     :param issue_number: integer
-#     :rtype: None or integer
-#     """
-#     number = None
-# 
-#     for issue in db.get_issues(state='open'):
-#         if issue.number == issue_number:
-#             number = issue.number
-#             break
-# 
-#     return number
-# 
-# def get_one_label(db, label_name, label_color):
-#     """
-#     :param db: :class:`github.Repository.Repository`
-#     :param label_name: string
-#     :param label_color: string
-#     :rtype: :class:`github.Label.Label`
-#     """
-# 
-#     if is_label_created(db, label_name):
-#         return db.get_label(label_name)
-#     else:
-#         return db.create_label(label_name, label_color)
-# 
 # def collect_one_label(db, list_of_labels, label_name, label_color):
 #     """
 #     :param db: :class:`github.Repository.Repository`
@@ -139,28 +29,6 @@ import shelve
 #         return db.get_milestone(milestone_number)
 #     else:
 #         return db.create_milestone(milestone_title)
-# 
-# 
-# def create_priority(args, db, list_of_labels):
-#     """
-#     :param args: :class:`argparse.Namespace`
-#     :param db: :class:`github.Repository.Repository`
-#     :param list_of_labels: list of :class:`github.Label.Label`s
-#     """
-#     if args.priority:
-#         prio = convert_priority_to_text(args.priority)
-#         collect_one_label(db, list_of_labels, prio, YELLOW)
-# 
-# def create_milestone(args, db):
-#     """
-#     :param args: :class:`argparse.Namespace`
-#     :param db: :class:`github.Repository.Repository`
-#     :rtype: :class:`github.Milestone.Milestone` or `github.GithubObject.NotSet`
-#     """
-#     if args.time:
-#         return get_one_milestone(db, convert_milestone_title(args.time))
-#     else:
-#         return github.GithubObject.NotSet
 # 
 # def create_listname(args, db, list_of_labels):
 #     """
@@ -198,27 +66,6 @@ import shelve
 #     # add new priority to task
 #     issue.add_to_labels(get_one_label(db, label_name, label_color))
 # 
-# def update_summary(issue, summary):
-#     """
-#     :param issue: :class:`github.Issue.Issue`
-#     :param summary: string
-#     """
-#     issue.edit(title=summary)
-# 
-# 
-        # print
-        # table_titles = ['TASK#', 'SUMMARY', 'LIST TYPE', 'DUE TIME', 'PRIORITY']
-        # print '{:<5}  {:<60}  {:<9}  {:<8}  {:<8}'.format(*table_titles)
-        # print '{:=<5}  {:=<60}  {:=<9}  {:=<8}  {:=<8}'.format(*['','','','',''])
-        # for issue in r.json():
-        #     for label in issue["labels"]:
-        #         if label["color"] == self.YELLOW: prio      = label["name"]
-        #         if label["color"] == self.GREEN:  list_type = label["name"]
-        #     print u'{:<5}  {:<60}  {:<9}  {:<8}  {:<8}'.format(issue["number"], 
-        #                                                        issue["title"], 
-        #                                                        list_type, 
-        #                                                        issue["milestone"]["title"], 
-        #                                                        prio)
 
 DEFAULT_BASE_URL = "https://api.github.com/repos/"
 REPO_NAME = 'keenhenry/todo'
@@ -248,6 +95,7 @@ class ListDB(object):
 
         self.__url_issues = DEFAULT_BASE_URL + REPO_NAME + "/issues"
         self.__auth       = (os.environ['PDA_AUTH'], '')
+        self.__max_taskno = -1
 
     @property
     def url_issues(self):
@@ -285,6 +133,44 @@ class ListDB(object):
         
         return prio, task_type
 
+    def _is_selected(self, task_type_in_db, task_type_requested, 
+                           milestone_in_db, milestone_requested, 
+                           priority_in_db, priority_requested):
+
+        return (task_type_requested is None or task_type_requested == task_type_in_db) and \
+               (milestone_requested is None or milestone_requested == milestone_in_db) and \
+               (priority_requested is None or priority_requested == priority_in_db)
+
+    def _print_header(self):
+
+        headers = ['TASK#', 'SUMMARY', 'LIST TYPE', 'DUE TIME', 'PRIORITY']
+
+        print
+        print '{:<5}  {:<60}  {:<9}  {:<8}  {:<8}'.format(*headers)
+        print '{:=<5}  {:=<60}  {:=<9}  {:=<8}  {:=<8}'.format(*['','','','',''])
+
+    def _print_task(self, db_local, task_number, task_type, milestone, priority):
+        """Helper function for read_tasks()
+        :param db_local: :class:`shelve.Shelf`
+        :param task_number: string
+        :param task_type: None or string
+        :param milestone: None or string
+        :param priority : None or string
+        """
+
+        assert db_local is not None and isinstance(db_local, shelve.Shelf), db_local
+
+        if self._is_selected(db_local[task_number]['type'], task_type,
+                             db_local[task_number]['milestone'], milestone,
+                             db_local[task_number]['priority'], priority):
+            print u'{:<5}  {:<60}  {:<9}  {:<8}  {:<8}'.format(
+                                                         task_number, 
+                                                         db_local[task_number]["summary"], 
+                                                         db_local[task_number]["type"], 
+                                                         db_local[task_number]["milestone"], 
+                                                         db_local[task_number]["priority"])
+
+
     def sync_local_dbstore(self):
 
         # retrieving OPEN issues from Github Issues
@@ -306,6 +192,8 @@ class ListDB(object):
                          }
 
             db_local[str(issue["number"])] = issue_data
+            self.__max_taskno = issue["number"] if issue["number"] > self.__max_taskno \
+                                                else self.__max_taskno
 
         # create a list to hold command history records
         db_local['CMDS_HISTORY'] = []
@@ -324,6 +212,9 @@ class ListDB(object):
         """
         :param task_number: integer
         """
+
+        assert isinstance(task_number, (int, long)), task_number
+
         # prepare a local db store for storing issues locally
         db_local = shelve.open(os.path.abspath(self.DEFAULT_LOCAL_DBPATH), 
                                protocol=-1, 
@@ -340,30 +231,101 @@ class ListDB(object):
 
         db_local.close()
 
-    def add_task(self):
-        print
+    def add_task(self, summary, task_type=None, milestone=None, priority=None):
+        assert summary   is not None and isinstance(summary,   str), summary
+        assert task_type is None or isinstance(task_type, str), task_type
+        assert milestone is None or isinstance(milestone, str), milestone
+        assert priority  is None or isinstance(priority,  str), priority
 
-    def edit_task(self):
-        print
+        issue_data = {
+                      "summary"  : summary,
+                      "type"     : task_type,
+                      "milestone": milestone,
+                      "priority" : priority
+                     }
 
-    def read_tasks(self):
+        db_local = shelve.open(os.path.abspath(self.DEFAULT_LOCAL_DBPATH), 
+                               protocol=-1,
+                               writeback=True)
+
+        # the value of the key for local store is not important, as long as it is unique
+        self.__max_taskno += 1
+        db_local[str(self.__max_taskno)] = issue_data
+
+        # record ADD operation in list 'CMDS_HISTORY' in local store
+        cmd_history_data = { 'CMD'      : 'ADD', 
+                             'SUMMARY'  : summary,
+                             'TYPE'     : task_type,
+                             'MILESTONE': milestone,
+                             'PRIORITY' : priority }
+        db_local['CMDS_HISTORY'].append(cmd_history_data)
+
+        db_local.close()
+
+    def edit_task(self, task_number, 
+                  new_summary  =None, 
+                  new_tasktype =None, 
+                  new_milestone=None, 
+                  new_priority =None):
+        """
+        :param task_number  : integer
+        :param new_summary  : string
+        :param new_tasktype : string
+        :param new_milestone: string
+        :param new_priority : string
+        """
+
+        assert task_number   is not None and isinstance(task_number, (int,long)), task_number
+        assert new_summary   is None or isinstance(new_summary,   str), new_summary
+        assert new_tasktype  is None or isinstance(new_tasktype,  str), new_tasktype
+        assert new_milestone is None or isinstance(new_milestone, str), new_milestone
+        assert new_priority  is None or isinstance(new_priority,  str), new_priority
+
+        db_local = shelve.open(os.path.abspath(self.DEFAULT_LOCAL_DBPATH), 
+                               protocol=-1,
+                               writeback=True)
+
+        if self._has_task(db_local, task_number):
+            if new_summary: 
+                db_local[str(task_number)]["summary"] = new_summary
+            if new_tasktype: 
+                db_local[str(task_number)]["type"] = new_tasktype
+            if new_milestone: 
+                db_local[str(task_number)]["milestone"] = new_milestone
+            if new_priority: 
+                db_local[str(task_number)]["priority"] = new_priority
+
+            # record EDIT operation in list 'CMDS_HISTORY' in local store
+            cmd_history_data = { 'CMD'      : 'EDIT', 
+                                 '#'        : task_number,
+                                 'SUMMARY'  : new_summary,
+                                 'TYPE'     : new_tasktype,
+                                 'MILESTONE': new_milestone,
+                                 'PRIORITY' : new_priority }
+            db_local['CMDS_HISTORY'].append(cmd_history_data)
+
+        db_local.close()
+
+    def read_tasks(self, task_type=None, milestone=None, priority=None):
+        """
+        :param task_type: None or string
+        :param milestone: None or string
+        :param priority : None or string
+        """
+
+        assert task_type is None or isinstance(task_type, str), task_type
+        assert milestone is None or isinstance(milestone, str), milestone
+        assert priority  is None or isinstance(priority,  str), priority
 
         db_local = shelve.open(os.path.abspath(self.DEFAULT_LOCAL_DBPATH), protocol=-1)
 
-        # print
-        # table_titles = ['TASK#', 'SUMMARY', 'LIST TYPE', 'DUE TIME', 'PRIORITY']
-        # print '{:<5}  {:<60}  {:<9}  {:<8}  {:<8}'.format(*table_titles)
-        # print '{:=<5}  {:=<60}  {:=<9}  {:=<8}  {:=<8}'.format(*['','','','',''])
-        # for key in db_local:
-        #     if key != 'CMDS_HISTORY':
-        #         print u'{:<5}  {:<60}  {:<9}  {:<8}  {:<8}'.format(key, 
-        #                                                            db_local[key]["summary"], 
-        #                                                            db_local[key]["type"], 
-        #                                                            db_local[key]["milestone"], 
-        #                                                            db_local[key]["priority"])
+        self._print_header()
+        for key in db_local:
+            if key != 'CMDS_HISTORY':
+                self._print_task(db_local, key, task_type, milestone, priority)
 
-        # for cmd in db_local['CMDS_HISTORY']:
-        #     print cmd
+        for cmd in db_local['CMDS_HISTORY']:
+            print cmd
 
         db_local.close()
 
@@ -374,12 +336,13 @@ def main():
 
     db.sync_local_dbstore()
 
-    # db.read_tasks()
-    # db.remove_task(37)
-    # db.read_tasks()
-    # db.remove_task(37)
-    # db.remove_task(39)
-    # db.read_tasks()
+    db.read_tasks()
+    db.add_task('task 6', 'tolearn', 'month', 'must')
+    db.read_tasks()
+    db.edit_task(task_number=44, new_tasktype='toread', new_milestone='year')
+    db.read_tasks()
+    # db.read_tasks('todo', 'day', 'high')
+    # db.read_tasks('todo', 'day', 'low')
     
     db.sync_remote_dbstore()
 
