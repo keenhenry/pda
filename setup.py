438efe3c4660a8df742ee13d18affb56387a6e57
Henry Huang <keenhenry1109@gmail.com>
2014-02-23 21:05:50 +0100

    added a few more named arguments to setup function, not completed yet

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@
 
 from distutils.core import setup
 
+long_description = open('README.rst').read()
+
 setup(
 name="pda",
-version="0.0.4",
 author="Henry Huang",
 packages=['listdb'],
+description='A command line tool managing all sorts of TODO lists',
+long_description=long_description,
+license='BSD',
+url='https://github.com/keenhenry/pda',
+version="0.0.4",
+platforms=['any'],
 scripts=["pda"]
 )
