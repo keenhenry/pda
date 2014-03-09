9a5ac68167aabe91b625c30dc68e4fa842d0cb6c
Henry Huang <keenhenry1109@gmail.com>
2014-03-09 15:25:19 +0100

    Added support for python2.6; added some more exceptions handling to deal with ill-formed configuration files. Close #38

--- pda/listdb/Config.py
+++ pda/listdb/Config.py
@@ -5,7 +5,7 @@
 
 """
 
-from ConfigParser import RawConfigParser, NoSectionError, DuplicateSectionError
+import ConfigParser
 import os
 
 class PdaConfig(object):
@@ -19,63 +19,67 @@ class PdaConfig(object):
 
     def __init__(self):
 
-        self.__config = RawConfigParser(self.DEFAULTS, allow_no_value=True)
-
-        # load configurations from several possible locations
-        self.__config.read(['./.pdaconfig', os.path.expanduser('~/.pdaconfig')])
+        try:
+            # load configurations from several possible locations
+            self.__config = ConfigParser.RawConfigParser(self.DEFAULTS)
+            self.__config.read(['./.pdaconfig', os.path.expanduser('~/.pdaconfig')])
+        except ConfigParser.ParsingError, err:
+            print 'pda:', err
+            print 'pda:', 'using default settings instead ...'
 
     @property
     def local_db_path(self):
         try:
             path = self.__config.get('pda', 'database-path')
-        except NoSectionError or DuplicateSectionError:
+        except ConfigParser.NoSectionError or ConfigParser.DuplicateSectionError:
             path = self.DEFAULTS['database-path']
-        return path
+
+        return path if path != "" else self.DEFAULTS['database-path']
 
     @property
     def username(self):
         try:
             name = self.__config.get('github', 'username')
-        except NoSectionError or DuplicateSectionError:
+        except ConfigParser.NoSectionError or ConfigParser.DuplicateSectionError:
             name = self.DEFAULTS['username']
-        return name
+
+        return name if name != "" else self.DEFAULTS['username']
 
     @property
     def reponame(self):
         try:
             name = self.__config.get('github', 'repo-name')
-        except NoSectionError or DuplicateSectionError:
+        except ConfigParser.NoSectionError or ConfigParser.DuplicateSectionError:
             name = self.DEFAULTS['repo-name']
-        return name
 
-    @property
-    def authtoken(self):
-        try:
-            token = self.__config.get('github', 'auth-token')
-        except NoSectionError or DuplicateSectionError:
-            token = self.DEFAULTS['auth-token']
-        return token
+        return name if name != "" else self.DEFAULTS['repo-name']
 
-    @property
-    def remote_mode(self):
-        try:
-            mode = (self.__config.get('github', 'username')   is not None) and \
-                   (self.__config.get('github', 'repo-name')  is not None) and \
-                   (self.__config.get('github', 'auth-token') is not None)
-        except NoSectionError or DuplicateSectionError:
-            mode = False
-        return mode
-
-    def change_reponame(self, new_reponame):
+    @reponame.setter
+    def reponame(self, new_reponame):
 
         assert new_reponame is not None and isinstance(new_reponame, str), new_reponame
 
         if self.remote_mode:
             self.__config.set('github', 'repo-name', new_reponame)
 
+    @property
+    def authtoken(self):
+        try:
+            token = self.__config.get('github', 'auth-token')
+        except ConfigParser.NoSectionError or ConfigParser.DuplicateSectionError:
+            token = self.DEFAULTS['auth-token']
+
+        return token if token != "" else self.DEFAULTS['auth-token']
+
+    @property
+    def remote_mode(self):
+        return (self.username is not None) and \
+               (self.reponame is not None) and \
+               (self.authtoken is not None)
+
 def main():
     cfg = PdaConfig()
-    cfg.change_reponame('todo')
+    cfg.reponame = 'todo'
     print cfg.username
     print cfg.reponame
     print cfg.authtoken
