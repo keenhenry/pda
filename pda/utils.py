292aa76a27538cbd73296d8b75198c215fe0608d
Henry Huang <keenhenry1109@gmail.com>
2014-03-13 21:57:11 +0100

    added die_msg function

--- pda/utils.py
+++ pda/utils.py
@@ -1,11 +1,13 @@
 """
 pda.utils
-~~~~~~~~~~~~~~
+~~~~~~~~~
 
 This module provides utility functions that are used within pda,
 that are also useful for external consumption, for example, unit tests.
 
 """
+
+import sys
 
 def cry_msg(prog, err_str="", msg=""):
     """Emit error messages for pda
@@ -14,3 +16,14 @@ def cry_msg(prog, err_str="", msg=""):
     :param msg:     string
     """
     print '{}: {}{}'.format(prog, err_str, msg)
+
+
+def die_msg(prog, msg=''):
+    """
+    :param prog:    string
+    :param err_str: string
+    :param msg:     string
+    """
+
+    print '{}: error: {}'.format(prog, msg)
+    sys.exit(1)
